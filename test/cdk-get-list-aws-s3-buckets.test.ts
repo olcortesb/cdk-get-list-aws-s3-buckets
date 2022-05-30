3b0be102e58d4ebc21fe860267ec99922d8fe283
Oscar Cortes <olcortesb@gmail.com>
2022-05-30 19:42:36 +0200

    feat: first commit project template

---
+++ test/cdk-get-list-aws-s3-buckets.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as CdkGetListAwsS3Buckets from '../lib/cdk-get-list-aws-s3-buckets-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/cdk-get-list-aws-s3-buckets-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new CdkGetListAwsS3Buckets.CdkGetListAwsS3BucketsStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
