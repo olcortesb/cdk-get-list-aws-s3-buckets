098245079fb084f00f5d5657e0d3fbdcfc4cfd23
Oscar Cortes <olcortesb@gmail.com>
2022-05-30 22:33:03 +0200

    feat: Add lambda and cofigure

--- lib/cdk-get-list-aws-s3-buckets-stack.ts
+++ lib/cdk-get-list-aws-s3-buckets-stack.ts
@@ -1,16 +1,45 @@
-import { Stack, StackProps } from 'aws-cdk-lib';
+import { Stack, StackProps, CfnOutput } from 'aws-cdk-lib';
 import { Construct } from 'constructs';
-// import * as sqs from 'aws-cdk-lib/aws-sqs';
+import { NodejsFunction } from "aws-cdk-lib/aws-lambda-nodejs"
+import { Runtime, FunctionUrlAuthType } from "aws-cdk-lib/aws-lambda";
+
+import * as iam from 'aws-cdk-lib/aws-iam';
+import * as path from 'path';
 
 export class CdkGetListAwsS3BucketsStack extends Stack {
   constructor(scope: Construct, id: string, props?: StackProps) {
     super(scope, id, props);
 
-    // The code that defines your stack goes here
+    const getListAwsS3Buckets = new NodejsFunction(this, "LambdaGetStatus", {
+      runtime: Runtime.NODEJS_14_X,
+      entry: path.join(__dirname, `/../functions/function.ts`),
+      handler: "handler"
+    });
 
-    // example resource
-    // const queue = new sqs.Queue(this, 'CdkGetListAwsS3BucketsQueue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
-    // });
+    //create a policy statement
+    const s3ListBucketsPolicy = new iam.PolicyStatement({
+      actions: ['s3:ListAllMyBuckets'],
+      resources: ['arn:aws:s3:::*'],
+    });
+
+    //add the policy to the Function's role
+    getListAwsS3Buckets.role?.attachInlinePolicy(
+      new iam.Policy(this, 'list-buckets-policy', {
+        statements: [s3ListBucketsPolicy],
+      }),
+    );
+
+    // add function url to lambda base
+    const urlgetListAwsS3Buckets = getListAwsS3Buckets.addFunctionUrl({
+      authType: FunctionUrlAuthType.NONE,
+      cors: {
+        allowedOrigins: ['*'],
+      }
+    });
+
+    // get url 
+    new CfnOutput(this, 'FunctionUrl', {
+      value: urlgetListAwsS3Buckets.url,
+    });
   }
 }
